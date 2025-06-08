3af05dd791532584d1a5022ec7aff1781f0fc934
frost.stamp@gmail.com <frost.stamp@gmail.com>
2025-06-08 18:18:11 +0300

    coments

--- script/bubbles.js
+++ script/bubbles.js
@@ -1,6 +1,12 @@
 
+/*   */
+
+/* Импортируемы ссылки  */
 import Spheres2Background from 'https://cdn.jsdelivr.net/npm/threejs-components@0.0.8/build/backgrounds/spheres2.cdn.min.js';
 
+
+
+/* Базовая часть кода JS  */
 const bg = Spheres2Background(document.getElementById('webgl-canvas'), {
    count: 200,
    colors: [0xff0000, 0x0, 0xffffff],
